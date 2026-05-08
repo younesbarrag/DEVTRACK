7674a777d5ba325dfab4dcca4fc4bf504ddf2378
younesbarrag <younesbarrag003@gmail.com>
2026-05-08 11:56:48 +0100

    fixcontrolle.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,18 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-<<<<<<< HEAD
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-=======
-
-abstract class Controller
-{
-    use AuthorizesRequests;
->>>>>>> feature/Controlles-files
 }
-
